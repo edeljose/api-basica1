432a73a35175f6c5426ce4474522a4e0838ea9a8
Edel Amaya <edeljose@gmail.com>
2025-04-10 20:45:39 -0500

    Versión inicial con Docker, tests y CI/CD 2

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => res.send('¡Hola Mundo desde Docker!'));
 app.get('/health', (req, res) => res.send({ status: 'ok' }));
 
-app.listen(PORT, () => console.log(`App corriendo en puerto ${PORT}`));
-
+if (require.main === module) {
+    app.listen(PORT, () => console.log(`App corriendo en puerto ${PORT}`));
+  }
 module.exports = app;
 
