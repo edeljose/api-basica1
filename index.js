4459e54de0f3e3dd9f63ab8ab51e41574ddab8a5
Edel Amaya <edeljose@gmail.com>
2025-04-15 20:55:09 -0500

    test con Eslint 2

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const express = require('express');
+import express from 'express';  // Error si estás usando 'require' y 'import'
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('¡Hola Mundo desde Docker! 1'));
 app.get('/health', (req, res) => res.send({ status: 'ok' }));
 
-if (require.main === module) {
-  app.listen(PORT, () => console.log(`App corriendo en puerto ${PORT}`));
-}
+console.log('Este es un log que no debería estar en producción'); // Error por `console.log()`
 
-module.exports = app;
+const unusedVariable = 'Esta variable nunca se usa'; // Error por variable no utilizada
+
+if (require.main === module) {
+    app.listen(PORT, () => console.log(`App corriendo en puerto ${PORT}`));
+}
+module.exports = app;  // Puede generar un error si estás usando ES Modules y 'module.exports'
 
